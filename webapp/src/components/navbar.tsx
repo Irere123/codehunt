3699f8f234ae3482f568f09ba3a283e486f6180b
Irere123 <irere2020@outlook.com>
2024-06-29 20:59:57 +0200

    feat(web): navbar icons

--- webapp/src/components/navbar.tsx
+++ webapp/src/components/navbar.tsx
@@ -1,5 +1,9 @@
 import { Link } from "@tanstack/react-router";
+
 import { useScroll } from "../hooks/use-scroll";
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "../lib/utils";
+import { SparklesIcon } from "./icons";
 
 export default function Navbar() {
   const scrolled = useScroll(50);
@@ -14,10 +18,21 @@ export default function Navbar() {
     >
       <div className="mx-5 flex h-16 w-full max-w-screen-md items-center justify-between">
         <Link href="/">
-          <img src="/logomark.svg" alt="OSS Gallery logo" className="h-14" />
+          <img src="/logomark.svg" alt="ReLaunch logo" className="h-14" />
         </Link>
         <div className="flex items-center space-x-2">
-          <button>Submit</button>
+          <a
+            href="https://github.com/irere123/codehunt"
+            target="_blank"
+            className={cn(
+              buttonVariants({ variant: "secondary" }),
+              "px-3 py-1.5"
+            )}
+          >
+            <SparklesIcon className="h-4 w-4" />
+            <p className="text-sm ml-2">{23}</p>
+          </a>
+          <Button>Submit</Button>
         </div>
       </div>
     </div>
