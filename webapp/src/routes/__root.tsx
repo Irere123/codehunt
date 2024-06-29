86300dcbd99f4c8e1b7d88343a762fcb69c27e4e
Irere123 <irere2020@outlook.com>
2024-06-29 19:19:31 +0200

    feat(web): react + vite + tanstack/router + tailwindcss

---
+++ webapp/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  notFoundComponent: () => {
+    return <p>This is the notFoundComponent configured on root route</p>;
+  },
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
