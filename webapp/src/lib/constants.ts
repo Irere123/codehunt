005e7cdcaa7fb1c2d80fba8482cf045aaf5cb0d1
Irere123 <irere2020@outlook.com>
2024-07-03 12:05:21 +0200

    prod api url

--- webapp/src/lib/constants.ts
+++ webapp/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const api_url = !__prod__
   ? "http://localhost:4000/graphql"
-  : "https://relaunch-latest.onrender.com";
+  : "https://relaunch-api.fly.dev";
